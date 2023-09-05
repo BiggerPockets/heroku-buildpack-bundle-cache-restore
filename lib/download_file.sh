7adb41b6d6e4b5a1d02bffa0676ad8900b88ff9a
Karl Entwistle <me@karlentwistle.com>
2023-09-05 11:36:41 +0100

    download file

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -3,16 +3,18 @@
 download_file () {
   local aws_binary_path=$1
   local remote_path=$2
-  local asset_manifest_path=$(get_env_variable ${env_dir} "ASSET_MANIFEST_PATH")
+  local bundle_cache_path=$3
+  local aws_bundle_cache_bucket=$(get_env_variable ${env_dir} "AWS_BUNDLE_CACHE_BUCKET")
 
   AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
     AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
-    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $build_dir/$asset_manifest_path --quiet
+    AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
+    $aws_binary_path s3 cp s3://${aws_bundle_cache_bucket}/$remote_path $bundle_cache_path --quiet
 
   local status=$?
 
   if [[ status -eq 0 ]]; then
-    echo "Downloaded remote $remote_path to $asset_manifest_path" | indent
+    echo "Downloaded remote $remote_path to $bundle_cache_path" | indent
   fi
 
   return $status
