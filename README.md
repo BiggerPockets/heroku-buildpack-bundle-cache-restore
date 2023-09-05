245aa3cdec4b264ec508756ae40c2aa19a463970
Karl Entwistle <me@karlentwistle.com>
2023-09-05 11:18:18 +0100

    Rebuild app

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Download asset manifest file from CDN host connected to Digital Ocean Spaces. As
     $ heroku buildpacks:add https://github.com/BiggerPockets/heroku-buildpack-bundle-cache-restore
     $ heroku config:add AWS_ACCESS_KEY_ID
     $ heroku config:add AWS_SECRET_ACCESS_KEY
+    $ heroku config:add AWS_REGION
     $ heroku config:add AWS_BUNDLE_CACHE_BUCKET
     $ heroku config:add AWS_BUNDLE_CACHE_PATH
---
+++ lib/check_file_exists.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+check_file_exists () {
+  local aws_binary_path=$1
+  local remote_path=$2
+  local aws_bundle_cache_bucket=$(get_env_variable ${env_dir} "AWS_BUNDLE_CACHE_BUCKET")
+
+  AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
+    AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
+    AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
+    $aws_binary_path s3api head-object --bucket "${aws_bundle_cache_bucket}" --key "${remote_path}" 2>/dev/null
+
+  local status=$?
+
+  if [[ status -eq 0 ]]; then
+    echo "File already exists in S3."
+  else
+    echo "File does not exist in S3."
+  fi
+
+  return $status
+}
