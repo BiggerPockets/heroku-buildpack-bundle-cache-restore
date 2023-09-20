d911eccfa1e31c588ddb8f79e33fe9c012104eb2
Karl Entwistle <me@karlentwistle.com>
2023-09-20 09:56:32 +0100

    Reduce outout from s3api

--- lib/check_file_exists.sh
+++ lib/check_file_exists.sh
@@ -8,14 +8,14 @@ check_file_exists () {
   AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
     AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
     AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
-    $aws_binary_path s3api head-object --bucket "${aws_bundle_cache_bucket}" --key "${remote_path}" 2>/dev/null
+    $aws_binary_path s3api head-object --bucket "${aws_bundle_cache_bucket}" --key "${remote_path}" > /dev/null
 
   local status=$?
 
   if [[ status -eq 0 ]]; then
-    echo "File already exists in S3."
+    echo "File ${remote_path} already exists in S3."
   else
-    echo "File does not exist in S3."
+    echo "File ${remote_path} does not exist in S3."
   fi
 
   return $status
